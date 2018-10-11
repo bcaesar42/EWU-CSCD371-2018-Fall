2c11dc5e64568ae5648a7f4c2464bd756224aa63
Bryan Caesar <bryancaesar@eagles.ewu.edu>
2018-10-10 22:31:31 -0700

    Commit before fetching new .gitignore file.(including HumanPlayer class)

---
+++ Assignment3/src/RockPaperScissors/HumanPlayer.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace RockPaperScissors
+{
+    public class HumanPlayer : MoveBehavior
+    {
+        public string MakeMove()
+        {
+            Console.Write("Please enter \"rock\", \"paper\", or \"scissors\": ");
+
+            return "test";
+        }
+    }
+}
