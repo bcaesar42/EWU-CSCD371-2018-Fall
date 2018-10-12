f5796346fc7c79af4ac526e298788458e6fe16b6
Bryan Caesar <bryancaesar@eagles.ewu.edu>
2018-10-11 23:53:59 -0700

    Player tests

---
+++ Assignment3/test/RockPaperScissors.Tests/GameTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RockPaperScissors.Tests
+{
+    [TestClass]
+    public class GamesTests
+    {
+        [TestMethod]
+        public void ProcessResultPlayer1Lost20Life()
+        {
+            string expected = "Player1 lost the round, and lost 20 life.";
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(expected, () =>
+                    Game.ProcessResult(new Player("Player1", new ComputerPlayer()), 20));
+        }
+    }
+}
