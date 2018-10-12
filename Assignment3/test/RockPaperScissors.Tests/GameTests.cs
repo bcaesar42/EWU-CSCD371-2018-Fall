36e14cb479aaa675d3697adfc8df7fbb7115ba30
Bryan Caesar <bryancaesar@eagles.ewu.edu>
2018-10-12 00:54:17 -0700

    For my own practice: Successfully implemented a test for the Game.PlayRound method.

--- Assignment3/test/RockPaperScissors.Tests/GameTests.cs
+++ Assignment3/test/RockPaperScissors.Tests/GameTests.cs
@@ -16,9 +16,18 @@ namespace RockPaperScissors.Tests
         [TestMethod]
         public void PlayRound2Humans()
         {
-            string expected = "Player1 lost the round, and lost 20 life.";
+            string p1Move = "rock";
+            string p2Move = "scissors";
+            string expected = $@"Please enter ""rock"", ""paper"", or ""scissors"": <<{p1Move}
+>>Please enter ""rock"", ""paper"", or ""scissors"": <<{p2Move}
+>>Player1 played: {p1Move}
+>>Player2 played: {p2Move}";
             IntelliTect.TestTools.Console.ConsoleAssert.Expect(expected, () =>
-                    Game.ProcessResult(new Player("Player1", new ComputerPlayer()), 20));
+                    {
+                        Player player1 = new Player("Player1", new HumanPlayer());
+                        Player player2 = new Player("Player2", new HumanPlayer());
+                        Game.PlayRound(player1, player2);
+                    });
         }
     }
 }
