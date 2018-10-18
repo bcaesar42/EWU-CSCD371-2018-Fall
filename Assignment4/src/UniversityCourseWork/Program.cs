c0d3f23a4dc1525b11c9bd293c59620ca3a9cc74
Bryan Caesar <bryancaesar@eagles.ewu.edu>
2018-10-18 14:47:52 -0700

    Commit to assignment4 branch.

---
+++ Assignment4/src/UniversityCourseWork/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UniversityCourseWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
