1c6941ce74591706d1dd540a4c7253a8707170fe
Bryan Caesar <bryancaesar@eagles.ewu.edu>
2018-10-20 23:54:03 -0700

    Done. (forgot to add test to commit)

---
+++ Assignment4/tests/UniversityCourseWork.Test/EventTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Assignment4.Tests
+{
+    [TestClass]
+    public class EventTests
+    {
+        [TestMethod]
+        public void Deconstruct_DefaultConstructor_DefaultValues()
+        {
+            Event testEvent = new Event();
+            Assert.AreEqual((null, new DateTime(2018, 1, 1)), testEvent.Deconstruct());
+        }
+    }
+}
