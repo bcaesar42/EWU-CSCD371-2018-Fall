d4122b9a7e33059adec6d37eb3ce57db2a15d78a
Bryan Caesar <bryancaesar@eagles.ewu.edu>
2018-10-20 18:35:07 -0700

    InstantiationCount implemented. (Including tests)

--- Assignment4/tests/UniversityCourseWork.Test/UniversityCourseTests.cs
+++ Assignment4/tests/UniversityCourseWork.Test/UniversityCourseTests.cs
@@ -7,9 +7,34 @@ namespace Assignment4.Tests
     public class UniversityCourseTests
     {
         [TestMethod]
+        public void InstantiationCount_4Gatherings_4()
+        {
+            Gathering gathering = new UniversityCourse();
+            gathering = new UniversityCourse("CS Course");
+            gathering = new Event();
+            gathering = new Event("Concert");
+
+            Assert.AreEqual(4, Gathering.InstantiationCount);
+        }
+
+        [TestMethod]
         public void Constructor_IsGathering_True()
         {
             Assert.IsInstanceOfType(new UniversityCourse(), typeof(Gathering));
         }
+
+        [TestMethod]
+        public void DefaultConstructor_GetName_Null()
+        {
+            Assert.IsNull(new UniversityCourse().GatheringName);
+        }
+
+        [TestMethod]
+        public void ExplictConstructor_GetName_CSCD371()
+        {
+            Assert.AreEqual(new UniversityCourse("CSCD371").GatheringName, "CSCD371");
+        }
+
+        
     }
 }
