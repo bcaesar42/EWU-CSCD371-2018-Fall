a7f61d1a04613c4aff1e98db5b456edf51af3289
Bryan Caesar <bryancaesar@eagles.ewu.edu>
2018-10-20 21:09:24 -0700

    CreditValue property implemented.

--- Assignment4/src/UniversityCourseWork/UniversityCourse.cs
+++ Assignment4/src/UniversityCourseWork/UniversityCourse.cs
@@ -6,10 +6,38 @@ namespace Assignment4
     {
         public UniversityCourse()
             : base(null)
-        {}
+        {
+            CreditValue = 0;
+        }
 
-        public UniversityCourse(string name)
+        public UniversityCourse(string name, int credits)
             : base(name)
-        {}
+        {
+            CreditValue = credits;
+        }
+
+        private int _CreditValue;
+        public int CreditValue
+        {
+            get
+            {
+                return _CreditValue;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Credits can not be negative.");
+                }
+                else if (value > 15)
+                {
+                    throw new ArgumentException("Too many credits for one course.");
+                }
+                else
+                {
+                    _CreditValue = value;
+                }
+            }
+        }
     }
 }
--- Assignment4/tests/UniversityCourseWork.Test/UniversityCourseTests.cs
+++ Assignment4/tests/UniversityCourseWork.Test/UniversityCourseTests.cs
@@ -1,5 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Assignment4;
+using System;
 
 namespace Assignment4.Tests
 {
@@ -10,7 +10,7 @@ namespace Assignment4.Tests
         public void InstantiationCount_4Gatherings_4()
         {
             Gathering gathering = new UniversityCourse();
-            gathering = new UniversityCourse("CS Course");
+            gathering = new UniversityCourse("CS Course", 4);
             gathering = new Event();
             gathering = new Event("Concert");
 
@@ -32,9 +32,37 @@ namespace Assignment4.Tests
         [TestMethod]
         public void ExplictConstructor_GetName_CSCD371()
         {
-            Assert.AreEqual(new UniversityCourse("CSCD371").GatheringName, "CSCD371");
+            Assert.AreEqual("CSCD371", new UniversityCourse("CSCD371", 5).GatheringName);
         }
 
-        
+        [TestMethod]
+        public void GetCredits_5CreditCourse_5()
+        {
+            UniversityCourse testCourse = new UniversityCourse("Test Course", 5);
+            Assert.AreEqual(5, testCourse.CreditValue);
+        }
+
+        [TestMethod]
+        public void SetCredits_SettingTo4_NoException()
+        {
+            UniversityCourse testCourse = new UniversityCourse("Test Course", 2);
+            testCourse.CreditValue = 4;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SetCredits_SetToNegative_Exception()
+        {
+            UniversityCourse testCourse = new UniversityCourse("Test Course", 2);
+            testCourse.CreditValue = -1;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SetCredits_SetTooLarge_Exception()
+        {
+            UniversityCourse testCourse = new UniversityCourse("Test Course", 2);
+            testCourse.CreditValue = 50;
+        }
     }
 }
