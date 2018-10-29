6d353d8bd4206243c860ec8959bc768e86abdbfa
Bryan Caesar <bryancaesar@eagles.ewu.edu>
2018-10-29 14:57:58 -0700

    Finished GetSelection method in Scheduler class.

--- Assignment5/src/UniversityCourseWork/Scheduler.cs
+++ Assignment5/src/UniversityCourseWork/Scheduler.cs
@@ -10,17 +10,52 @@ namespace Assignment5
 
             do
             {
-
+                PrintOptions();
+                selection = GetSelection();
             } while (selection != 0);
         }
 
         private static void PrintOptions()
         {
-            Console.WriteLine("1.) Print current schedule.");
-            Console.WriteLine("2.) Add university course.");
-            Console.WriteLine("3.) Add one-time event.");
+            MyConsole terminal = new MyConsole();
 
-            Console.WriteLine("0.) Exit.");
+            terminal.WriteLine("1.) Print current schedule.");
+            terminal.WriteLine("2.) Add university course.");
+            terminal.WriteLine("3.) Add one-time event.");
+            terminal.WriteLine("0.) Exit.");
+        }
+
+        private static int GetSelection()
+        {
+            MyConsole terminal = new MyConsole();
+            int selection = -1;
+            string userInput;
+
+            do
+            {
+                terminal.Write("Please enter the number of your selection: ");
+                userInput = terminal.ReadLine();
+
+                try
+                {
+                    selection = int.Parse(userInput);
+
+                    if (selection < 0 || selection > 3)
+                    {
+                        throw new ArgumentException("Invalid Input - Number not in valid range.");
+                    }
+                }
+                catch (FormatException)
+                {
+                    terminal.WriteLine("Invalid Input - Not a number.");
+                }
+                catch (ArgumentException e)
+                {
+                    terminal.WriteLine(e.Message);
+                }
+            } while (selection < 0 || selection > 3);
+
+            return selection;
         }
     }
 }
