5f20d373217a080af5e14d391e2836ca5e898d7e
unknown <bryancaesar@eagles.ewu.edu>
2018-10-30 23:54:22 -0700

    MyConsole test.

---
+++ Assignment5/tests/UniversityCourseWork.Test/CalendarTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment5.Tests
+{
+    [TestClass]
+    public class CalendarTests
+    {
+        [TestMethod]
+        public void TestPrintOptions()
+        {
+            string expected = @"----------------------------------------------------
+1.) Print current schedule.
+2.) Add university course.
+3.) Add one-time event.
+0.) Exit.";
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(expected, Calendar.PrintOptions);
+        }
+    }
+}
