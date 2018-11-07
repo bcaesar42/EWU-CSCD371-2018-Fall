b149f2affa6c17e59989cf31582cf88e5dd940ae
unknown <bryancaesar@eagles.ewu.edu>
2018-11-06 21:38:42 -0800

    Added WeekDays enum.

---
+++ Assignment6/src/UniversityCourseWork/WeekDays.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment6
+{
+    [Flags]
+    public enum WeekDays
+    {
+        Monday = 1,
+        Tuesday = 2,
+        Wednesday = 4,
+        Thursday = 8,
+        Friday = 16,
+        Saturday = 32,
+        Sunday = 64
+    }
+}
