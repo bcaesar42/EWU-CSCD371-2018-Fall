4af6e92f309966f6346847c399c426974ceb3038
unknown <bryancaesar@eagles.ewu.edu>
2018-11-06 23:48:23 -0800

    Added extra class and struct.

---
+++ Assignment6/src/UniversityCourseWork/BehaviorClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment6
+{
+    public class BehaviorClass
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+    }
+}
---
+++ Assignment6/src/UniversityCourseWork/BehaviorStruct.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment6
+{
+    public class BehaviorStruct
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+    }
+}
