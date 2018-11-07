ecac1eee7baed7e44cf2f3cfd166f3917803cf9f
unknown <bryancaesar@eagles.ewu.edu>
2018-11-06 23:59:05 -0800

    Tests.

---
+++ Assignment6/tests/UniversityCourseWork.Test/BehaviorsTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Runtime.InteropServices;
+
+namespace Assignment6.Tests
+{
+    [TestClass]
+    public class BehaviorsTests
+    {
+        private void ChangeStructAge(BehaviorStruct str)
+        {
+            str.age = 102;
+        }
+
+        private void ChangeStructAgeByRef(ref BehaviorStruct str)
+        {
+            str.age = 102;
+        }
+
+        [TestMethod]
+        public void Struct_ValueNotChangedAfterMethod()
+        {
+            BehaviorStruct behave = new BehaviorStruct("Bob", 42);
+            ChangeStructAge(behave);
+            Assert.AreEqual(42, behave.age);
+        }
+
+        [TestMethod]
+        public void Struct_ValueIsChangedAfterMethod()
+        {
+            BehaviorStruct behave = new BehaviorStruct("Bob", 42);
+            ChangeStructAge(behave);
+            Assert.AreEqual(102, behave.age);
+        }
+    }
+}
